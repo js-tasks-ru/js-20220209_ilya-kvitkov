37b0f7d28c097ad99855e57f739163a667e277c6
Квитков Илья <kvitkovii@it2g.ru>
2022-02-19 19:43:14 +0300

    03-objects-arrays-intro-to-testing/3-trim-symbols

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,26 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
-
+  if (size === 0) {
+    return "";
+  }
+  if (!size) {
+    return string;
+  }
+  const splitedArray = string.split("");
+  let counter = 0;
+  const newArray = splitedArray.map((item, index) => {
+    const nextIsEqual = splitedArray[index + 1] !== splitedArray[index];
+    if (counter < size) {
+      ++counter;
+      if (nextIsEqual) {
+        counter = 0;
+      }
+      return item;
+    }
+    if (nextIsEqual) {
+      counter = 0;
+    }
+  });
+  return newArray.join("");
 }
